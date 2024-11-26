c5e701c4ba7296a4d957df3678ac8de7a9685a12
AkaneDev <akane@akanedev.au>
2024-11-26 12:45:52 +1000

    Restarting THe Mod on fabric Dont download this its an empty mod

--- src/main/java/au/akanedev/zombiesurvival/Zombiesurvival.java
+++ src/main/java/au/akanedev/zombiesurvival/Zombiesurvival.java
@@ -1,10 +1,18 @@
 package au.akanedev.zombiesurvival;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Zombiesurvival implements ModInitializer {
+    public static final String MODID = "zombiesurvival";
+    public static final String MODDEVNAME = "AkaneDev";
+    Logger LOGGER = LogManager.getLogger(MODID);
+    Logger LogDev = LogManager.getLogger(MODDEVNAME);
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Hello I have been initialized!, 'Im dyslexic' - Noah");
+        LogDev.info("If you have this mod and i didnt give it to you, then you dont deserve it");
     }
 }
