d3633edd8000e843a2c5171dd8c703b88c67158e
AkaneDev <akane@akanedev.au>
2024-11-26 12:07:07 +1000

    Restarting THe Mod on fabric Dont download this its an empty mod

---
+++ src/main/java/au/akanedev/zombiesurvival/Zombiesurvival.java
@@ -0,0 +1,10 @@
+package au.akanedev.zombiesurvival;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Zombiesurvival implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
